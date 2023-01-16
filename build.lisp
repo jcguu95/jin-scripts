f721974d3b0c7080349c86437dcf9578656320e8
jcguu95 <jcguu95@gmail.com>
2023-01-15 23:39:14 -0600

    Add thin shell script wrappers.

--- build.lisp
+++ build.lisp
@@ -1,5 +1,22 @@
 (ql:quickload :jin-scripts)
 
+;; TODO Abstract the directory /Users/jin/.local/bin/
+
+(let ((commands (alexandria:hash-table-keys
+                 jin-scripts::*registered-commands*)))
+  (loop for command in commands do
+    (let ((file-path (format nil "/Users/jin/.local/bin/cl.~a" command)))
+      (with-open-file (file file-path
+                            :direction :output
+                            :if-does-not-exist :create
+                            :if-exists :overwrite)
+        (format file "#!/bin/bash~%~%")
+        (format file "cl ~a \"$@\"~%~%" command)
+        (format file "exit"))
+      (uiop:run-program
+       (format nil "chmod 744 ~a" file-path)
+       :output t :error-output t))))
+
 (sb-ext:save-lisp-and-die
  "/Users/jin/.local/bin/cl"
  :toplevel 'jin-scripts:main
